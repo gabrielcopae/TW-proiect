00b0ca7e0df2b0b2d191faa8e01bee4f8ebcad9d
gabrielcopae <copaegabriel>
2021-12-02 21:54:07 +0200

    a

---
+++ proiecte.js
@@ -0,0 +1,10 @@
+class Proiect {
+    constructor (Id,nume,domeniu,echipa){
+        this.id = id;
+        this.nume = nume;
+        this.domeniu = domeniu;
+        this.echipa = echipa;
+    }
+}
+
+module.exports = Proiect;
